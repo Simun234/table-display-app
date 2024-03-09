f8b5854c2f71ca809ee1e85469284d5741c661e4
Šimun <simun.hordov5@gmail.com>
2024-03-09 11:51:21 +0100

    Add data fetching and pagination logic
    Implemented functionality to fetch data from a JSON file using axios in the DataTable component. Added pagination logic to display a specified number of items per page.

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 const DataTable = () => {
   const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
   const itemsPerPage = 24;
+
+  useEffect(() => {
+    axios
+      .get("./data.json")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentPageData = data.slice(indexOfFirstItem, indexOfLastItem);
 };
 
 export default DataTable;
